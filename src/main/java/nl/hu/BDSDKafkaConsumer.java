adffc137df3205850013249eeb99fe8f947931ec
Roelant Ossewaarde <roelanto@gmail.com>
2019-12-19 10:59:41 +0100

    Rename of the classes to BDSD*

--- src/main/java/nl/hu/BDSDKafkaConsumer.java
+++ src/main/java/nl/hu/BDSDKafkaConsumer.java
@@ -1,5 +1,7 @@
 package nl.hu;
 
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
 import java.util.HashSet;
 import java.util.Properties;
 import java.util.Set;
@@ -8,25 +10,23 @@ import java.util.Set;
  * Hello world!
  *
  */
-public class KafkaConsumer extends Thread {
-        private final String topic = "";
-        private final Boolean isAsync = false;
-        private static final String KAFKA_SERVER_URL = "localhost";
-        private static final int KAFKA_SERVER_PORT = 9092;
-        private static final String CLIENT_ID = "BDSDKafkaProducer";
-        private static final Set<String> filenames = new HashSet<>();
+public class BDSDKafkaConsumer extends Thread {
+    private final String topic;
+    private final Boolean isAsync;
+    private final KafkaConsumer<Integer, String> kafkaConsumer;
+    private static final String KAFKA_SERVER_URL = "localhost";
+    private static final int KAFKA_SERVER_PORT = 9092;
+    private static final String CLIENT_ID = "BDSDKafkaProducer";
 
-    public KafkaConsumer(String topic, boolean isAsync) {
+    public BDSDKafkaConsumer(String topic, boolean isAsync) {
         Properties properties = new Properties();
         properties.put("bootstrap.servers", KAFKA_SERVER_URL + ":" + KAFKA_SERVER_PORT);
         properties.put("client.id", CLIENT_ID);
         properties.put("key.serializer", "org.apache.kafka.common.serialization.IntegerSerializer");
         properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        private final KafkaConsumer<Integer, String> consumer = KafkaConsumer<Integer, String>();
+        kafkaConsumer = new KafkaConsumer <Integer, String>(properties);
         this.topic = topic;
         this.isAsync = isAsync;
-        this.filenames.addAll(filenames);
-
     }
 
 
--- src/main/java/nl/hu/BDSDKafkaConsumerRunner.java
+++ src/main/java/nl/hu/BDSDKafkaConsumerRunner.java
@@ -3,7 +3,7 @@ package nl.hu;
 /**
  * Created by roelant on 19/12/2019.
  */
-public class KafkaConsumerRunner {
+public class BDSDKafkaConsumerRunner {
     public static final String TOPIC = "transactie";
 
     public static void main(String[] args) {
